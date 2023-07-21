4e5e5b7fd274c8a925dff521eda3686f159199f6
amwpurdue <amwpurdue@gmail.com>
2023-07-21 11:32:32 -0500

    connect to mongodb

--- ims-api.py
+++ ims-api.py
@@ -1,10 +1,34 @@
 from flask import Flask
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+def get_database():
+    if ("db_root_password" in os.environ):
+        client = MongoClient(
+            host=os.environ["MY_RELEASE_MONGODB_SERVICE_HOST"] + ":" + os.environ["MY_RELEASE_MONGODB_SERVICE_PORT"],
+            username="root",
+            password=os.environ["db_root_password"]
+        )
+    else:
+        client = MongoClient(
+            host=os.environ["MY_RELEASE_MONGODB_SERVICE_HOST"] + ":" + os.environ["MY_RELEASE_MONGODB_SERVICE_PORT"]
+        )
+
+    return client[os.environ["db_name"]]
+
 @app.route("/")
 def index():
-    return "ims-api index page"
+    try:
+        db = get_database()
+        users = db["users"].find()
+        count = 0
+        for user in users:
+            count += 1
+        return "ims-api index page: Connection to mongodb successful. Users: " + str(count)
+    except:
+        return "ims-api index page: Connection to mongodb failed"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
