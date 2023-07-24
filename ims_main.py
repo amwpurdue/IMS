2f9cb8c124730120cfe2ca3cceeba38860ce6769
amwpurdue <amwpurdue@gmail.com>
2023-07-24 11:12:30 -0500

    basic frontend to show all products in a table

--- ims_main.py
+++ ims_main.py
@@ -14,11 +14,26 @@ def index():
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("index.html")
 
-    indexDict = {
+    template_dict = {
         "computerName": socket.gethostname() + " (" + socket.gethostbyname("localhost") + ")"
     }
 
-    return template.render(indexDict)
+    return template.render(template_dict)
+
+
+@app.route("/front")
+def frontend():
+    environment = Environment(loader=FileSystemLoader("templates/"))
+    template = environment.get_template("front.html")
+
+    products_json = ims_api.get_products().json
+    template_dict = {
+        "computerName": socket.gethostname() + " (" + socket.gethostbyname("localhost") + ")",
+        "products": products_json["products"]
+    }
+    print(template_dict)
+    return template.render(template_dict)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
